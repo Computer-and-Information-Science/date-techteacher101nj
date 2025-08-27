7434adb6986de8487a99eb5d2236c84627402e95
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2025-08-27 19:21:27 +0000

    Oops I forgot my message

--- Date.java
+++ Date.java
@@ -1,5 +1,12 @@
 public class Date {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       String day = "Wednesday";
+       String date = "27";
+       String month = "August";
+       String year = "2025";
+
+       System.out.println("American Format :" + day + "," + " " + month + " " + date + ", "+ year);
+       System.out.println("European Format :" + day + " " + date + " " + month + " "+ year);
+ 
     }
 }
